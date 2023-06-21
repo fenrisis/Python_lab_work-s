6ec3891d1a8b75f74d2bb67d91d26794377db2f2
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2023-06-21 03:13:36 +0200

    folowing & profile
    - added a models foe folowing & profile
    - added a templates
    - updated a views

--- project4/network/views.py
+++ project4/network/views.py
@@ -1,10 +1,14 @@
+import json
+from django.core.paginator import Paginator
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
-
-from .models import User
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
+from .models import User, Post, Follow, Like
 
 
 def index(request):
@@ -61,3 +65,50 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+def following(request):
+    user = request.user
+    following = Follow.objects.filter(follower=request.user).values('following_id')
+
+    posts = Post.objects.filter(user__in=following).order_by('-timestamp')
+    for post in posts:
+        post.likes = Like.objects.filter(post=post.id).count()
+        post.save()
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "network/following.html", {
+        "page_obj": page_obj
+    })
+
+def profile(request, owner):
+    owner = User.objects.get(id=owner)
+    button = "Follow" if Follow.objects.filter(follower=request.user, following=owner).count() == 0 else "Unfollow"
+
+    if request.method == "POST":
+        if request.POST["button"] == "Follow":
+            button = "Unfollow"
+            Follow.objects.create(follower=request.user, following=owner)
+        else:
+            button = "Follow"
+            Follow.objects.get(follower=request.user, following=owner).delete()
+        
+    posts = Post.objects.filter(user=owner.id).order_by('-timestamp')
+    for post in posts:
+        post.likes = Like.objects.filter(post=post.id).count()
+        post.save()
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "network/profile.html", {
+        "owner": owner, 
+        "followers": Follow.objects.filter(following=owner).count(), 
+        "following": Follow.objects.filter(follower=owner).count(), 
+        "page_obj": page_obj, 
+        "button": button
+    })
