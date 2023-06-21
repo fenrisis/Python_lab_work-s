0b4907a347eb60b5c28b9724bd5305bdc04d62d8
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2023-06-21 03:43:55 +0200

    js added
    -added a javascript for edit & like
    -edit & like views

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("following", views.following, name="following"),
     path("profile/<str:owner>", views.profile, name="profile"),
+
+    # api
+    path("edit/<str:post_id>", views.edit, name="edit"), 
+    path("like/<str:post_id>", views.like, name="like")
 ]
